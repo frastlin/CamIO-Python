f56028d0ee684b31476f460d38faeb1aaed7a0fa
Brandon Biggs <brandonkeithbiggs@gmail.com>
2019-01-03 18:54:09 -0800

    added an AdvancedGrid and a sound object that stays in space.

--- playground/playground_map.py
+++ playground/playground_map.py
@@ -1,19 +1,29 @@
 import pyaudiogame
 from pyaudiogame import speak as spk
 from pyaudiogame.ui.grid import AdvancedGrid
+from pyaudiogame.mixer import Sound, get_listener
 my_app = pyaudiogame.App("Playground Test")
 
 grid = AdvancedGrid(step_sounds=["grid_sounds/step2.ogg", "grid_sounds/step3.ogg"], hit_sounds=["grid_sounds/hit.ogg"])
 
+sound1 = Sound("playground_sounds/bench.ogg", position=[10,10])
+
 def callback2():
 	spk("Fire")
 
+def on_move2(event):
+	pass
 
 
-grid.add_polygon([(4,4), (5, 7), (8,7), (8,4)], callback=callback2)
+
+grid.add_polygon([(4,4), (5, 7), (8,7), (8,4)], callback=callback2, on_move=on_move2)
 
 @my_app.add_handler
 def on_input(event):
 	grid.move(event)
+	if event.key and event.state:
+		 if event.key == "space":
+			 spk(str(get_listener()))
 
+sound1.play(loops=-1)
 my_app.run()
